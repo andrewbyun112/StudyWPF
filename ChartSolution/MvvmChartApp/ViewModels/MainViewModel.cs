632f3d6aae93aa8bf38d10a9a76209869cfc95bc
seungjae <nimbus112@naver.com>
2020-08-03 16:20:49 +0900

    0803WPFChart

---
+++ ChartSolution/MvvmChartApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Windows.Shapes;
+
+namespace MvvmChartApp.ViewModels
+{
+    public class MainViewModel : Conductor<object>
+    {
+        public void LoadLineChart()
+        {
+            ActivateItem(new LineChartViewModel());
+        }
+
+        public void LoadGaugeChart()
+        {
+            ActivateItem(new GaugeChartViewModel());
+        }
+
+        public void ExitProgram()
+        {
+            Environment.Exit(0); // 0은 오류없이 끝내기
+        }
+
+    }
+}
